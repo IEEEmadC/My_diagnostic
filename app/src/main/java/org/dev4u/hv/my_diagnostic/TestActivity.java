5aff0df4d092aefae22cb745b9d9e15e7421f412
jonathan_geovany <hv12i04001@usonsonate.edu.sv>
2017-06-12 11:16:43 -0600

    commit inicial

---
+++ app/src/main/java/org/dev4u/hv/my_diagnostic/TestActivity.java
@@ -0,0 +1,13 @@
+package org.dev4u.hv.my_diagnostic;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+    }
+}
