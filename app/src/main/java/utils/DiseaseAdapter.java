4460be0f9920c0cdfbf2feddeb6179fd51ab427b
jonathan_geovany <hv12i04001@usonsonate.edu.sv>
2017-06-26 11:03:03 -0600

    Se Muestran iconos de acorde a la categoria

--- app/src/main/java/utils/DiseaseAdapter.java
+++ app/src/main/java/utils/DiseaseAdapter.java
@@ -2,6 +2,8 @@ package utils;
 
 import android.content.Context;
 import android.content.res.ColorStateList;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.graphics.Typeface;
 import android.support.v7.widget.CardView;
@@ -19,6 +21,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Filter;
 import android.widget.Filterable;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import org.dev4u.hv.my_diagnostic.R;
@@ -40,12 +43,29 @@ public class DiseaseAdapter extends RecyclerView.Adapter<DiseaseAdapter.DiseaseV
     private ArrayList<Disease> mOriginalValues = new ArrayList<>();
     boolean isSearch=false;
     String textSearch="";
+    ArrayList<Integer> lstIcons = new ArrayList<>();
+
+
 
     public DiseaseAdapter(Context mContext, ArrayList<Disease> diseaseArrayList, boolean isSearch) {
         this.mContext = mContext;
         this.diseaseArrayList = diseaseArrayList;
         this.mOriginalValues = diseaseArrayList;
         this.isSearch = isSearch;
+        lstIcons.add(R.drawable.ic_cat_1);
+        lstIcons.add(R.drawable.ic_cat_2);
+        lstIcons.add(R.drawable.ic_cat_3);
+        lstIcons.add(R.drawable.ic_cat_4);
+        lstIcons.add(R.drawable.ic_cat_5);
+        lstIcons.add(R.drawable.ic_cat_6);
+        lstIcons.add(R.drawable.ic_cat_7);
+        lstIcons.add(R.drawable.ic_cat_8);
+        lstIcons.add(R.drawable.ic_cat_9);
+        lstIcons.add(R.drawable.ic_cat_10);
+        lstIcons.add(R.drawable.ic_cat_11);
+        lstIcons.add(R.drawable.ic_cat_12);
+        lstIcons.add(R.drawable.ic_cat_13);
+        lstIcons.add(R.drawable.ic_cat_14);
     }
 
     @Override
@@ -74,13 +94,22 @@ public class DiseaseAdapter extends RecyclerView.Adapter<DiseaseAdapter.DiseaseV
         holder.description.setText("Description : "+descriptionBuild);
         holder.symptomsCount.setText("Symptoms : "+disease.getSymptoms().size());
         holder.category.setText("Category : " + disease.getCategory_name());
-        holder.percentage.setText("");
-        holder.symptomsMatch.setText("");
+        //holder.percentage.setText("");
+        //holder.symptomsMatch.setText("");
         if(isSearch){
             holder.percentage.setText(new DecimalFormat("###.##").format(disease.getMatchPercentage()));
-            holder.symptomsMatch.setText("Maatches : "+disease.getSymptoms_match());
+            holder.symptomsMatch.setText("Matches : "+disease.getSymptoms_match());
             //holder.symptomsMatch.setText(disease.);
         }
+
+        int id_category = Integer.parseInt(disease.getId_disease_category());
+        Log.d("id guardado : ","======== "+id_category);
+        if(id_category>0 && id_category<lstIcons.size()){
+            holder.imgDisease.setImageResource(lstIcons.get(id_category-1));
+            //Log.d("id guardado : ","mostrado ======== "+id_category+" w "+lstIcons.get(id_category-1).getWidth());
+            //holder.imgDisease.setImageBitmap(lstIcons.get(id_category-1));
+        }
+
     }
 
     @Override
@@ -118,15 +147,15 @@ public class DiseaseAdapter extends RecyclerView.Adapter<DiseaseAdapter.DiseaseV
                     for (int i = 0; i < mOriginalValues.size(); i++) {
                         String data = mOriginalValues.get(i).getName_disease();
                         if (data.toLowerCase().startsWith(constraint.toString())) {
-                            FilteredArrList.add(
-                                    new Disease(
-                                            mOriginalValues.get(i).getId_disease(),
-                                            mOriginalValues.get(i).getName_disease(),
-                                            mOriginalValues.get(i).getDescription(),
-                                            mOriginalValues.get(i).getId_disease_category(),
-                                            mOriginalValues.get(i).getSymptoms()
-                                    )
+                            Disease disease = new Disease(
+                                    mOriginalValues.get(i).getId_disease(),
+                                    mOriginalValues.get(i).getName_disease(),
+                                    mOriginalValues.get(i).getDescription(),
+                                    mOriginalValues.get(i).getId_disease_category(),
+                                    mOriginalValues.get(i).getSymptoms()
                             );
+                            disease.setCategory_name(mOriginalValues.get(i).getCategory_name());
+                            FilteredArrList.add(disease);
                         }
                     }
                     // set the Filtered result to return
@@ -153,6 +182,7 @@ public class DiseaseAdapter extends RecyclerView.Adapter<DiseaseAdapter.DiseaseV
         public TextView description;
         public TextView category;
         public CardView cardView;
+        public ImageView imgDisease;
 
         public DiseaseViewHolder(View view) {
             super(view);
@@ -163,7 +193,7 @@ public class DiseaseAdapter extends RecyclerView.Adapter<DiseaseAdapter.DiseaseV
             symptomsMatch = (TextView) view.findViewById(R.id.lblSymptomsMatches);
             category = (TextView) view.findViewById(R.id.lblCategory);
             cardView=(CardView)view.findViewById(R.id.item_symptom);
-
+            imgDisease = (ImageView) view.findViewById(R.id.img_disease);
         }
     }
 }
