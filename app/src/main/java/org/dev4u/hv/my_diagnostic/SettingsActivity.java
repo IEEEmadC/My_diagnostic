b578dbfaf6b393d0e481d7fc878fe180abe595dd
jonathan_geovany <hv12i04001@usonsonate.edu.sv>
2017-07-11 20:44:35 -0600

    tamaño del toggle modificado

--- app/src/main/java/org/dev4u/hv/my_diagnostic/SettingsActivity.java
+++ app/src/main/java/org/dev4u/hv/my_diagnostic/SettingsActivity.java
@@ -1,13 +1,22 @@
 package org.dev4u.hv.my_diagnostic;
 
+import android.graphics.drawable.AnimationDrawable;
+import android.graphics.drawable.StateListDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ToggleButton;
 
 public class SettingsActivity extends AppCompatActivity {
+
+    ToggleButton toggleSettings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        toggleSettings = (ToggleButton) findViewById(R.id.toggleUpdates);
+        //StateListDrawable stateListDrawable = (StateListDrawable) toggleSettings.getBackground();
+        //AnimationDrawable animationDrawable = (AnimationDrawable) stateListDrawable.getCurrent();
+        //animationDrawable.selectDrawable(animationDrawable.getNumberOfFrames() - 1);
     }
 }
